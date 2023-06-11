9940237f24c481be3d6929d066662c7d78460b4a
Treaty <135383414+treaty1210@users.noreply.github.com>
2023-06-11 13:54:12 -0700

    Pass all test conditions
    Update for loop code to accept larger numbers first to fix issue of
    undefined being returned.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -2,32 +2,29 @@ const sumAll = function(first, last) {
     let answer = 0;
     if (first < 0 || last < 0) {
         return "ERROR";
-    }
-    if (!Number.isInteger(first) || !Number.isInteger(last)) {
+    } else if (!Number.isInteger(first) || !Number.isInteger(last)) {
         return "ERROR";
     }
-    if (last > first) {
+    //This code uses a math formula to find sum, but it does not add up integers to find answer.
+    /* else if (last > first) {
         answer = (((last * (last + 1))) / 2);
         return answer;
     } else {
         answer = (((first * (first + 1))) / 2);
         return answer;
 }
-}   
-    
-    //This one uses a for loop, does not work with large number first.
-    /* if (last > first) {
+}   */
+    //This code adds all the integers to find the sum.
+     else if (last > first) {
         for (i = first; i < last + 1; i++) {
             answer += i;
         }
-        return answer;
-    } else if (first > last) {
+    } else {
         for (i = last; i < first + 1; i++) {
             answer += i;
         }
-    } else {
-        return "ERROR";
-    }
-}; */
+    } 
+    return answer;
+};
 // Do not edit below this line
 module.exports = sumAll;
