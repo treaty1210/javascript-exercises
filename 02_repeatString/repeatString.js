13df1ad9eda2dd9ffddd7364991ae8a238719660
Treaty <135383414+treaty1210@users.noreply.github.com>
2023-06-10 17:49:35 -0700

    Pass test 1
    Created string var that holds the str value.
    Created a for loop that adds str value to string given specified num.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function(str, num) {
+    let string = "";
+    for (i = 0; i < num; i++) {
+        string += str;
+    }
+    return string;
 };
 
 // Do not edit below this line
