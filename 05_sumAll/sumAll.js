00b95b3fc2d255543ae485c8c6e1e67e0446f51e
Treaty <135383414+treaty1210@users.noreply.github.com>
2023-06-11 12:13:00 -0700

    Run test for first two in sumAll.spec.js
    Set up for loop for test one.
    For loop did not work for test two.
    Rewrote formula that applies to both.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,10 @@
-const sumAll = function() {
-
+const sumAll = function(first, last) {
+    let answer = "";
+    answer = (((last * (last + 1)))/ 2)
+    //for (i = first; i <= last + 1; i++) {
+    //    answer = (i + (i + 1) - 1); 
+    //}
+    return answer;
 };
 
 // Do not edit below this line
