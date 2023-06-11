78a0cbdc8e69cdf3fe91aeee85010c728addf9d9
Treaty <135383414+treaty1210@users.noreply.github.com>
2023-06-10 20:50:37 -0700

    Complete removeFromArray
    Set up a function that removes multiple items from arrays.
    The filter was used to return a new array based on new values without
    the specified item.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,5 @@
-const removeFromArray = function() {
-
-};
-
+const removeFromArray = function(array, ...num) {
+    return array.filter (item => !num.includes(item))
+}
 // Do not edit below this line
 module.exports = removeFromArray;
