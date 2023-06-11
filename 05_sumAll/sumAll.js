97306c2efae38ce0cd1d14dc8645783596c13899
Treaty <135383414+treaty1210@users.noreply.github.com>
2023-06-11 13:32:17 -0700

    Add new conditions to function
    Added new conditions to function to return ERROR when non-integers are
    used.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -3,16 +3,19 @@ const sumAll = function(first, last) {
     if (first < 0 || last < 0) {
         return "ERROR";
     }
-       if (last > first) {
-            answer = (((last * (last + 1))) / 2);
-            return answer;
-        } else {
-            answer = (((first * (first + 1))) / 2);
-            return answer;
+    if (!Number.isInteger(first) || !Number.isInteger(last)) {
+        return "ERROR";
     }
+    if (last > first) {
+        answer = (((last * (last + 1))) / 2);
+        return answer;
+    } else {
+        answer = (((first * (first + 1))) / 2);
+        return answer;
+}
 }   
     
-    //This one uses a for loop
+    //This one uses a for loop, does not work with large number first.
     /* if (last > first) {
         for (i = first; i < last + 1; i++) {
             answer += i;
