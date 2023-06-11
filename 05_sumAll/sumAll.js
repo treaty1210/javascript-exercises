899ef3a65d09fe74662901cd4ab79b3663245a1e
Treaty <135383414+treaty1210@users.noreply.github.com>
2023-06-11 13:25:12 -0700

    Adjust code to return ERROR on negative numbers
    Adjust for loop to make cleaner
        Does not work with large numbers first
    
    Adjust non-for loop to account for negative numbers.
        Now returns error if first or last value contains a value < 0.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,11 +1,30 @@
 const sumAll = function(first, last) {
-    let answer = "";
-    answer = (((last * (last + 1)))/ 2)
-    //for (i = first; i <= last + 1; i++) {
-    //    answer = (i + (i + 1) - 1); 
-    //}
-    return answer;
-};
-
+    let answer = 0;
+    if (first < 0 || last < 0) {
+        return "ERROR";
+    }
+       if (last > first) {
+            answer = (((last * (last + 1))) / 2);
+            return answer;
+        } else {
+            answer = (((first * (first + 1))) / 2);
+            return answer;
+    }
+}   
+    
+    //This one uses a for loop
+    /* if (last > first) {
+        for (i = first; i < last + 1; i++) {
+            answer += i;
+        }
+        return answer;
+    } else if (first > last) {
+        for (i = last; i < first + 1; i++) {
+            answer += i;
+        }
+    } else {
+        return "ERROR";
+    }
+}; */
 // Do not edit below this line
 module.exports = sumAll;
