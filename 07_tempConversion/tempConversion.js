6cb56ba09b8762c9fe5796576afa09201f62cbe8
Treaty <135383414+treaty1210@users.noreply.github.com>
2023-06-11 17:20:25 -0700

    Create function for temp conversion to test
    Create function using formulas for temperature conversion between
    Celsius and Fahrenheit.
    
    Run tests for all conditions to ensure it can work with negatives and
    rounds to 1 decimal space.

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  let roundTemp = ((temp - 32) * (5/9));
+  return Math.round(roundTemp * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  let roundTemp = ((temp * (9/5)) + 32);
+  return Math.round(roundTemp * 10) / 10;
 };
 
 // Do not edit below this line
