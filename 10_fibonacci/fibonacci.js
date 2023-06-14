0d33e18fd81524e55568500632d58e94fac03087
Treaty <135383414+treaty1210@users.noreply.github.com>
2023-06-14 14:26:56 -0700

    Create function for fibonacci
    Use for loop to push new arguments into array to create the sequence.

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,9 @@
-const fibonacci = function() {
-
+const fibonacci = function(a) {
+    const sequence = [0, 1]; //set up an array
+    for (i = 1; i < a; i++) { //i = 1 so that fib sequence starts at 1 instead of 0
+        sequence.push(sequence[i] + sequence[i - 1]); //push new value to end of array and the first value would be 1 + 0 = 1, etc.
+    }
+    return sequence[a]; //return certain number in the array
 };
 
 // Do not edit below this line
