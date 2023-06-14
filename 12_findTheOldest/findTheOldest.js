78b6fbb4f8ee704722a45452d54133b21ae54ace
Treaty <135383414+treaty1210@users.noreply.github.com>
2023-06-14 15:02:28 -0700

    Create function that finds oldest
    Use reduce method to find oldest person.
    Create function to determine ages.
    Create a death year for those that are living.

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,17 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(array) {
+    return array.reduce((oldest, currentPerson) => {
+        const oldAge = currentAge(oldest.yearOfBirth, oldest.yearOfDeath); //Run first object in array through currentAge function
+        const age = currentAge(currentPerson.yearOfBirth, currentPerson.yearOfDeath); //Run next object through currentAge function
+        return oldAge < age ? currentPerson : oldest; //If oldAge is less than age, then return currentPerson. If not then return oldest
+    });
 };
+
+const currentAge = (birth, death) => { //Age function, should be called in the first function to determine age
+    if (!death) {
+        death = new Date().getFullYear(); //This solves issue of Carly not having death year
+    }
+    return death - birth;
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
