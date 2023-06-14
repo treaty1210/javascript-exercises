901916a5bbcdd0c672119fe274b365c622cc6ec7
Treaty <135383414+treaty1210@users.noreply.github.com>
2023-06-14 14:11:54 -0700

    Rewrite function to pass other tests
    Strip out all space, capitals, and punctuation and then run through
    reverse string function.
    
    Compare it with stripped version to reversed version and if same return
    true.

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,10 +1,7 @@
 const palindromes = function (word) {
-    let palindrome = word.split("").reverse().join("");
-    if (palindrome === word) {
-        return true;
-    }
-        
-};
+    let palindrome = word.toLowerCase().replace(/[^a-z0-9]/g, ""); //removes all uppercase, spaces, and punctuation
+    return palindrome.split("").reverse().join("") === palindrome; //reverse string function tested against original palindrome
+}; 
 
 // Do not edit below this line
 module.exports = palindromes;
