de9fa3e1301d8ba0ec84cc085620c428a33ad528
Treaty <135383414+treaty1210@users.noreply.github.com>
2023-06-14 12:45:49 -0700

    Create factorial function
    If factorial intial value is 0, set it equal to 1.
    Otherwise, run for loop that talks value and multiplies it with single
    decreasing value to get answer.

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -21,12 +21,22 @@ const multiply = function(...a) {
 };
   
 
-const power = function() {
-	
+const power = function(a, b) {
+	let powAnswer = a;
+  for (i = 1; i < b; i++) {
+    powAnswer *= a; 
+  }
+  return powAnswer;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  let facAnswer = 1;
+	if (a === 0) {
+    return 1;
+  } for (i = a; i > 0; i--) {
+    facAnswer *= i;
+  } 
+  return facAnswer;
 };
 
 // Do not edit below this line
