d6a32d810b8722e587f0bdbdd3392b6b54198fea
Treaty <135383414+treaty1210@users.noreply.github.com>
2023-06-14 14:28:50 -0700

    Update to not work with negatives
    Return OOPS if there is a negative in the argument.
    
    Function should already work with strings.

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,7 @@
 const fibonacci = function(a) {
+    if (a < 0) {
+        return "OOPS";
+    }
     const sequence = [0, 1]; //set up an array
     for (i = 1; i < a; i++) { //i = 1 so that fib sequence starts at 1 instead of 0
         sequence.push(sequence[i] + sequence[i - 1]); //push new value to end of array and the first value would be 1 + 0 = 1, etc.
